228b150d601dbc64e85799affeeb90f81ef3f58e
Emanuel Palm <emanuel+github@pipe.how>
2023-05-09 20:23:39 +0200

    version bump 2.2.0, build removes debug symbols if needed

--- build.ps1
+++ build.ps1
@@ -2,6 +2,9 @@ param(
     [ValidateSet('Debug', 'Release')]
     [string]
     $Configuration = 'Release',
+    
+    [switch]
+    $NoDebugSymbols,
 
     [string]
     $Version,
@@ -34,7 +37,11 @@ if (-not $NoClean.IsPresent) {
     dotnet build-server shutdown
     dotnet clean -c $Configuration
 }
-dotnet publish -c $Configuration
+if ($NoDebugSymbols.IsPresent) {
+    dotnet publish -c $Configuration /p:DebugType=None /p:DebugSymbols=false
+} else {
+    dotnet publish -c $Configuration
+}
 
 # Ensure output directories exist and are clean for build
 New-Item -Path $OutDir -ItemType Directory -ErrorAction Ignore
